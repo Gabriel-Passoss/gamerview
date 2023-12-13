852a1936ca759a85f969f2b2b66b796b9b59867a
Gabriel dos Passos <kooujinproductions@gmail.com>
2023-12-13 20:03:11 -0300

    add: setters, createdAt, updatedAt and playing attributes

--- src/domain/review/enterprise/entities/reviewer.ts
+++ src/domain/review/enterprise/entities/reviewer.ts
@@ -1,5 +1,6 @@
 import { Entity } from '@/core/entities/entity'
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Game } from './game'
 
 export interface ReviewerProps {
   name: string
@@ -8,6 +9,9 @@ export interface ReviewerProps {
   profileImageUrl: string
   birthday: Date
   subtitle: string
+  playing: Game[]
+  createdAt: Date
+  updatedAt: Date
 }
 
 export class Reviewer extends Entity<ReviewerProps> {
@@ -15,16 +19,40 @@ export class Reviewer extends Entity<ReviewerProps> {
     return this.props.name
   }
 
+  set name(name: string) {
+    this.props.name = name
+
+    this.touch()
+  }
+
   get email() {
     return this.props.email
+  }
+
+  set email(email: string) {
+    this.props.email = email
+
+    this.touch()
   }
 
   get nickname() {
     return this.props.nickname
   }
 
+  set nickname(nickname: string) {
+    this.props.nickname = nickname
+
+    this.touch()
+  }
+
   get profileImageUrl() {
     return this.props.profileImageUrl
+  }
+
+  set profileImageUrl(url: string) {
+    this.props.profileImageUrl = url
+
+    this.touch()
   }
 
   get birthday() {
@@ -33,6 +61,16 @@ export class Reviewer extends Entity<ReviewerProps> {
 
   get subtitle() {
     return this.props.subtitle
+  }
+
+  set subtitle(subtitle: string) {
+    this.props.subtitle = subtitle
+
+    this.touch()
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
   }
 
   static create(props: ReviewerProps, id?: UniqueEntityID) {
