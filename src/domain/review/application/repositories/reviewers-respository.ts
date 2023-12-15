8916d56b22c3e4f6783b89d564aea4699ef60067
Gabriel dos Passos <kooujinproductions@gmail.com>
2023-12-15 16:07:57 -0300

    add: findByEmail on reviewers repository

--- src/domain/review/application/repositories/reviewers-respository.ts
+++ src/domain/review/application/repositories/reviewers-respository.ts
@@ -4,6 +4,7 @@ import { Reviewer, ReviewerProps } from '../../enterprise/entities/reviewer'
 export abstract class ReviewersRepository {
   abstract findById(id: string): Promise<Reviewer | null>
   abstract findByNickname(nickname: string): Promise<Reviewer | null>
+  abstract findByEmail(email: string): Promise<Reviewer | null>
   abstract findManyByFollowers(params: PaginationParams): Promise<Reviewer[]>
   abstract findManyByFollowing(params: PaginationParams): Promise<Reviewer[]>
   abstract create(reviewer: ReviewerProps): Promise<void>
