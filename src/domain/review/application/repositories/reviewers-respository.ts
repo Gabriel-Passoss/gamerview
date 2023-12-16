9284ab9b325f1f27eb731f91eda6f1f447653889
Gabriel dos Passos <kooujinproductions@gmail.com>
2023-12-16 15:43:34 -0300

    fix: reviewers repository create and save input type

--- src/domain/review/application/repositories/reviewers-respository.ts
+++ src/domain/review/application/repositories/reviewers-respository.ts
@@ -1,12 +1,9 @@
-import { PaginationParams } from '@/core/repositories/pagination-params'
-import { Reviewer, ReviewerProps } from '../../enterprise/entities/reviewer'
+import { Reviewer } from '../../enterprise/entities/reviewer'
 
 export abstract class ReviewersRepository {
   abstract findById(id: string): Promise<Reviewer | null>
   abstract findByNickname(nickname: string): Promise<Reviewer | null>
   abstract findByEmail(email: string): Promise<Reviewer | null>
-  abstract findManyByFollowers(params: PaginationParams): Promise<Reviewer[]>
-  abstract findManyByFollowing(params: PaginationParams): Promise<Reviewer[]>
-  abstract create(reviewer: ReviewerProps): Promise<void>
-  abstract save(reviewer: ReviewerProps): Promise<void>
+  abstract create(reviewer: Reviewer): Promise<void>
+  abstract save(reviewer: Reviewer): Promise<void>
 }
