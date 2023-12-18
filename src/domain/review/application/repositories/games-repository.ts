371d2863d608e3d767d87cb4150931fe984457b4
Gabriel dos Passos <kooujinproductions@gmail.com>
2023-12-18 15:36:22 -0300

    add: changes on game sand review comments repositories

--- src/domain/review/application/repositories/games-repository.ts
+++ src/domain/review/application/repositories/games-repository.ts
@@ -1,7 +1,12 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { Game } from '../../enterprise/entities/game'
 
 export abstract class GamesRepository {
   abstract findById(id: string): Promise<Game | null>
-  abstract findManyByTitle(title: string): Promise<Game[]>
+  abstract findManyByTitle(
+    title: string,
+    params: PaginationParams,
+  ): Promise<Game[]>
+
   abstract create(game: Game): Promise<void>
 }
--- src/domain/review/application/repositories/review-comments-repository.ts
+++ src/domain/review/application/repositories/review-comments-repository.ts
@@ -3,8 +3,11 @@ import { ReviewComment } from '../../enterprise/entities/review-comment'
 
 export abstract class ReviewCommentsRepository {
   abstract findById(id: string): Promise<ReviewComment | null>
-  abstract findBySlug(slug: string): Promise<ReviewComment | null>
-  abstract findManyRecent(params: PaginationParams): Promise<ReviewComment[]>
+  abstract findManyByReviewId(
+    reviewId: string,
+    params: PaginationParams,
+  ): Promise<ReviewComment[]>
+
   abstract create(reviewComment: ReviewComment): Promise<void>
   abstract save(reviewComment: ReviewComment): Promise<void>
   abstract delete(reviewComment: ReviewComment): Promise<void>
