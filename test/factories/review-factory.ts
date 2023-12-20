e0c1bdd29db314c929a28f15fc6a4021cb74e652
Gabriel dos Passos <kooujinproductions@gmail.com>
2023-12-20 17:59:23 -0300

    add: game attribute on review factory

--- test/factories/review-factory.ts
+++ test/factories/review-factory.ts
@@ -2,6 +2,7 @@ import { faker } from '@faker-js/faker'
 
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { Review } from '@/domain/review/enterprise/entities/review'
+import { makeGame } from './game-factory'
 
 export function makeReview(
   override: Partial<Review> = {},
@@ -11,6 +12,7 @@ export function makeReview(
     {
       reviewerId: new UniqueEntityID(),
       title: faker.lorem.sentence(),
+      game: makeGame(),
       content: faker.lorem.text(),
       hoursOfGameplay: Math.floor(Math.random() * (200 - 50 + 1)) + 50,
       isCompleted: true,
