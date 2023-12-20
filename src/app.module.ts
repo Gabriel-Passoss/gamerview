0cdd7b9aecda270cace5d72320156eeaf70c0a4b
Gabriel dos Passos <kooujinproductions@gmail.com>
2023-12-20 18:09:52 -0300

    feat: env validation

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,13 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { envSchema } from './infra/env/env'
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
 })
 export class AppModule {}
