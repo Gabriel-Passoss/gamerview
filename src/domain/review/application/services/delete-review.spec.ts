6a7bbffc6b9a93222f1f8e0c790901b59019fa41
Gabriel dos Passos <kooujinproductions@gmail.com>
2023-12-31 15:38:06 -0300

    add: reviewerId validation on delete review

--- src/domain/review/application/services/delete-review.spec.ts
+++ src/domain/review/application/services/delete-review.spec.ts
@@ -2,6 +2,7 @@ import { InMemoryReviewsRepository } from 'test/repositories/in-memory-reviews-r
 import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 import { DeleteReviewService } from './delete-review'
 import { makeReview } from 'test/factories/review-factory'
+import { makeReviewer } from 'test/factories/reviewer-factory'
 
 let inMemoryReviewsRepository: InMemoryReviewsRepository
 let sut: DeleteReviewService
@@ -13,11 +14,16 @@ describe('Delete review', () => {
   })
 
   it('should be able to delete a review', async () => {
-    const review = makeReview({}, new UniqueEntityID('review-1'))
+    const reviewer = makeReviewer()
+    const review = makeReview(
+      { reviewerId: reviewer.id },
+      new UniqueEntityID('review-1'),
+    )
     await inMemoryReviewsRepository.create(review)
 
     const result = await sut.execute({
       reviewId: 'review-1',
+      reviewerId: reviewer.id.toString(),
     })
 
     expect(result.isRight()).toBe(true)
--- src/domain/review/application/services/delete-review.ts
+++ src/domain/review/application/services/delete-review.ts
@@ -1,10 +1,11 @@
 import { Either, left, right } from '@/core/either'
-import { Injectable } from '@nestjs/common'
+import { Injectable, UnauthorizedException } from '@nestjs/common'
 import { ResourceNotFoundError } from './errors/resource-not-found-error'
 import { ReviewsRepository } from '../repositories/reviews-repository'
 
 interface DeleteReviewServiceRequest {
   reviewId: string
+  reviewerId: string
 }
 
 type DeleteReviewServiceResponse = Either<ResourceNotFoundError, null>
@@ -15,11 +16,15 @@ export class DeleteReviewService {
 
   async execute({
     reviewId,
+    reviewerId,
   }: DeleteReviewServiceRequest): Promise<DeleteReviewServiceResponse> {
     const reviewToDelete = await this.reviewsRepository.findById(reviewId)
 
     if (!reviewToDelete) {
       return left(new ResourceNotFoundError())
+    }
+    if (reviewToDelete.reviewerId.toString() !== reviewerId) {
+      return left(new UnauthorizedException())
     }
 
     await this.reviewsRepository.delete(reviewToDelete)
